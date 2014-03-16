52ee4768cf65e23b3cfddb9d757c5e803e69ffc7
rhyswynne <rhyswynne@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-16 19:23:00 +0000

    First Upload (1.0)
    git-svn-id: https://plugins.svn.wordpress.org/wp-flipclock/trunk@876336 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: WP Flipclock
+Plugin URI: http://winwar.co.uk/plugins/wp-flipclock/
+Description: Adds a jQuery Flipclock to the site (based on flipclock.js library)
+Version: 1.0
+Author: Winwar Media
+Author URI: http://winwar.co.uk/
+License: GPL2
+*/
+
+define('WPFLIP_WIDGET_PLUGIN_PATH', dirname(__FILE__));
+require_once(WPFLIP_WIDGET_PLUGIN_PATH . '/inc/core.php');
+
+?>
